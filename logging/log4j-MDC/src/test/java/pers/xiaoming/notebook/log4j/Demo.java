46de15455add3410cc758b68267b7d211b35eb69
Xiaoming Sun <rsun2014@gmail.com>
2019-07-11 17:29:02 -0700

    More comment and tests/demos for log4j MDC

--- logging/log4j-MDC/src/test/java/pers/xiaoming/notebook/log4j/Demo.java
+++ logging/log4j-MDC/src/test/java/pers/xiaoming/notebook/log4j/Demo.java
@@ -22,11 +22,48 @@ public class Demo {
     @Test
     public void MDCGetterDemo() throws InterruptedException {
         for (int i = 0; i < NUM_THREADS; i++) {
+
+            // It is confusing, because here is the main/test thread put the value
+            // the executor thread shouldn't have access to the main/test thread's MDC which is threadlocal
             MDC.put("user_id", i);
             MDC.put("user_name", "myname" + i);
 
             MDCGetterDemoRunner runner = new MDCGetterDemoRunner(countDownLatch);
             executor.submit(runner);
+        }
+
+        countDownLatch.await();
+    }
+
+    @Test
+    public void MDCGetterCorrectDemo() throws InterruptedException {
+        for (int i = 0; i < NUM_THREADS; i++) {
+
+            final int index = i;
+            MDCGetterDemoRunner runner = new MDCGetterDemoRunner(countDownLatch);
+            executor.submit(() -> {
+                MDC.put("user_id", index);
+                MDC.put("user_name", "myname" + index);
+                runner.run();
+            });
+        }
+
+        countDownLatch.await();
+    }
+
+    @Test
+    public void MDCGetterPutTwiceTest() throws InterruptedException {
+        for (int i = 0; i < NUM_THREADS; i++) {
+            MDC.put("user_id", "main/test:" + i);
+            MDC.put("user_name", "main/test:" + i);
+
+            final int index = i;
+            MDCGetterDemoRunner runner = new MDCGetterDemoRunner(countDownLatch);
+            executor.submit(() -> {
+                MDC.put("user_id", Thread.currentThread().getName() + ":" + index);
+                MDC.put("user_name", Thread.currentThread().getName() + ":" + index);
+                runner.run();
+            });
         }
 
         countDownLatch.await();
