c6d82bf4b14a256296eda2e4fbe334ba84ec9af8
Xiaoming Sun <rsun2014@gmail.com>
2019-08-20 16:38:11 -0700

    Dependency management for junit and testng modules

--- testng/src/test/java/pers/xiaoming/notebook/testng/depend/DependTestAmend.java
+++ testng/src/test/java/pers/xiaoming/notebook/testng/depend/DependTestAmend.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 @Test(groups = "func_test")
 public class DependTestAmend {
-    @Test(dependsOnMethods = "pers.xiaoming.notebook.depend.DependTest.func_step_2")
+    @Test(dependsOnMethods = "pers.xiaoming.notebook.testng.depend.DependTest.func_step_2")
     public void func_step_3() {
         System.out.println("Running func test 3");
     }
